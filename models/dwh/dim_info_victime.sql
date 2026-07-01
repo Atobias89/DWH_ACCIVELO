16f00d414f7ee6b106f31cde4a93812132a27e5e
alex <elvoga.09@gmail.com>
2026-07-01 16:21:28 +0200

    dim info victime implementation

---
+++ models/dwh/dim_info_victime.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = 'accident_identity',
+        incremental_strategy = 'merge',
+        merge_exclude_columns = ['created_at'],
+        on_schema_change = 'append_new_columns'
+    )
+}}
+
+select
+       {{dbt_utils.generate_surrogate_key([
+        'victime_id',
+        'gravite_accident'
+    ])}} as victime_identity,
+    victime_id,
+    gravite_accident,
+    sexe,
+    trajet_victime,
+    equipement_securite,
+    date_accident,
+    created_at,
+    updated_at
+from {{ref('stg_info_victime')}}
+
+ {%  if is_incremental() %}
+    where date_accident > (select coalesce(max(date_accident), '1900-01-01') from {{this}})
+{% endif %}
